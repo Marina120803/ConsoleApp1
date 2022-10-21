66754f5cddd2f072885ac631ed2d2a0fe3370dd6
ACER <ACER@DESKTOP-O7A416V>
2022-10-21 22:57:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace aip;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите N судей="); // Ввод количества судей
+        int n = int.Parse(Console.ReadLine());
+
+        double[] marks = new double[n];
+
+        double max = 0;
+        double min = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            marks[i] = new Random().Next(10);
+        }
+
+        min = marks[0];
+
+        for (int i = 0; i < n; i++)
+        {
+
+            if (marks[i] > max)
+                max = marks[i];
+
+            if (marks[i] < min)
+                min = marks[i];
+
+        }
+
+        int count = 0;
+        double sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            if (marks[i] != min && marks[i] != max)
+            {
+                count++;
+                sum += marks[i];
+            }
+        }
+
+        Console.WriteLine("Среднее арифметическое оценок = {0}", sum / count); // Вывод на экран среднего арифметического оценок
+
+    }
+}
